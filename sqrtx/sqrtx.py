9b467c1ae8a24b41475fd22333fbc25f6bc0426b
Dev Chaudhary <102573059+Dev5203@users.noreply.github.com>
2023-06-09 23:12:08 +0530

    Time: 16 ms (90.49%) | Memory: 13.4 MB (17.19%) - LeetSync

---
+++ sqrtx/sqrtx.py
@@ -0,0 +1,11 @@
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        z=x
+        return int(math.sqrt(x))
+        """
+        :type x: int
+        :rtype: int
+        """
+
+
