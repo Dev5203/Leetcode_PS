3a2ad9ba42f935c2a843b3757be429afe04abcc4
Dev Chaudhary <102573059+Dev5203@users.noreply.github.com>
2023-06-27 20:23:29 +0530

    Time: 191 ms (34.26%) | Memory: 15.3 MB (75.01%) - LeetSync

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        n=[]
+        for i in nums:
+            n.append(i*i)
+        return sorted(n)
+
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
