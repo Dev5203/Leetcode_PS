76b547e78e4274de5cd0672049050d14eb44ba58
Dev Chaudhary <102573059+Dev5203@users.noreply.github.com>
2023-07-01 11:49:28 +0530

    Time: 134 ms (83.91%) | Memory: 15 MB (62.69%) - LeetSync

---
+++ majority-element/majority-element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        nums.sort()
+        return nums[len(nums)//2]
+
+        
+       
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
