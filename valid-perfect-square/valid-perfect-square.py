1a92b0a6bdcfe7343e139720a1d127800fa816df
Dev Chaudhary <102573059+Dev5203@users.noreply.github.com>
2023-07-01 13:24:46 +0530

    Time: 11 ms (91.58%) | Memory: 13.3 MB (33.38%) - LeetSync

---
+++ valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        
+        if int(num**0.5)*int(num**0.5)==num:
+			return True
+		
+			
+        """
+        :type num: int
+        :rtype: bool
+        """
+        
