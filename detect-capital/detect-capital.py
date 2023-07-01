a27cebccdd8610e65be00d82e464d4e9668b992c
Dev Chaudhary <102573059+Dev5203@users.noreply.github.com>
2023-07-01 14:41:04 +0530

    Time: 17 ms (62.77%) | Memory: 13.4 MB (72.62%) - LeetSync

---
+++ detect-capital/detect-capital.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        return word.isupper() or word.islower() or word.istitle()
+      
+
+        """
+        :type word: str
+        :rtype: bool
+        """
