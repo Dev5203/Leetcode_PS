bfc96989966ffd815b2fe6c495849ded3b590c88
Dev Chaudhary <102573059+Dev5203@users.noreply.github.com>
2023-09-20 18:17:20 +0530

    Time: 157 ms (35.06%) | Memory: 15.4 MB (60.13%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -3,11 +3,10 @@ class Solution(object):
         n=[]
         for i in nums:
             n.append(i*i)
-        for j in range(len(n)):
-            min_val=min(n[j:])
-            min_ind=n.index(min_val,j)
-            n[j],n[min_ind]=n[min_ind],n[j]
-        return n
+        return sorted(n)
+        
+        
+
         
         
         
