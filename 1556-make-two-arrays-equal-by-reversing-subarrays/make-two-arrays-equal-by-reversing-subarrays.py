191d2923ddedd282d56065b3a103bd146e520f25
Dev Chaudhary <102573059+Dev5203@users.noreply.github.com>
2024-05-04 12:45:41 +0530

    Time: 55 ms (64.00%) | Memory: 11.9 MB (61.14%) - LeetSync

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canBeEqual(self, target, arr):
+        target.sort()
+        arr.sort()
+        if arr==target:
+            return True
+        else:
+            return False
+
+        """
+        :type target: List[int]
+        :type arr: List[int]
+        :rtype: bool
+        """
+        
